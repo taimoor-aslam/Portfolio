34797a212a79d640e51c85c9440ac8ef27edc150
Taimoor Aslam <taslam525@gmail.com>
2023-02-12 13:43:11 +0500

    first commit

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,52 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hello and welcome to my portfolio! I am a full-stack developer with
+            expertise in the MERN (MongoDB, Express, React, and Node.js) stack.
+            I have a passion for creating dynamic and interactive web
+            applications that provide intuitive and seamless user experiences.
+            With my strong knowledge of the MERN stack, I have been able to
+            develop various applications for clients, ranging from simple
+            content management systems to complex e-commerce platforms. In
+            addition to my technical skills, I am a dedicated and
+            detail-oriented individual who takes pride in delivering
+            high-quality work. I have excellent communication skills, and I am
+            comfortable working both independently and as part of a team. Please
+            take a look at some of my projects in my portfolio to get a better
+            understanding of my skills and experience. If you have any questions
+            or would like to discuss a potential project, please do not hesitate
+            to reach out.
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Writting Tech Blogs
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer">Taimoor</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,88 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+import Remember from "../../Assets/Projects/remember.webp";
+import KinFolk from "../../Assets/Projects/kinfolk.png";
+import MazGlobal from "../../Assets/Projects/maz-global.png";
+import DfsArmy from "../../Assets/Projects/dfs-army.png";
+import Pernia from "../../Assets/Projects/pernia.png";
+import ForestTown from "../../Assets/Projects/foresttown.png";
+
+function Projects() {
+  const projects = [
+    {
+      imgPath: KinFolk,
+      title: "KINFOLK",
+      description:
+        "A web application built with MERN stack which is used to integrate various HR functions into a unified system that shares data and outputs reports across the entire platform as a core functionality.",
+      demoLink: "",
+    },
+    {
+      imgPath: DfsArmy,
+      title: "DFS ARMY",
+      description:
+        "A web application built with MEAN stack which is a research site, lineup optimizer tool, and collaboration platform bundled into one. It's a popular solution for DFS players looking to both prepare for weekly slates and improve their performance in daily fantasy contests.",
+      demoLink: "https://stage.dfsarmy.com/",
+    },
+    {
+      imgPath: MazGlobal,
+      title: "MAZ GLOBAL",
+      description:
+        "A web application built with MERN stack, MAZ Global is an online auto parts store, operating as a global wholesale and retail provider of aftermarket auto parts. They are the trusted suppliers of automotive spare parts with a focus on delivering high-quality goods at competitive prices.",
+      demoLink: "https://www.mazglobal.co.uk/",
+    },
+    {
+      imgPath: Pernia,
+      title: "PERNIA",
+      description:
+        "A web application built with MERN stack, A cloth brand website.",
+      demoLink: "https://www.mazglobal.co.uk/",
+    },
+    {
+      imgPath: Remember,
+      title: "REMEMBER",
+      description:
+        "A Flutter mobile app to hold precious memories that start young, to cherish and remember!",
+      demoLink:
+        "https://play.google.com/store/apps/details?id=com.applicity.remember&pli=1",
+    },
+    {
+      imgPath: ForestTown,
+      title: "Forest Town",
+      description:
+        "A web application built with MERN stack, A housing scheme in Pakistan offering promising biophilic concepts and architectures in the ethos of the urban community.  Biophilia is currently the most sought and popular concept among town planners and organizations.",
+      demoLink:
+        "https://play.google.com/store/apps/details?id=com.applicity.remember&pli=1",
+    },
+  ];
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          {projects.map((project) => {
+            return (
+              <Col md={4} className="project-card">
+                <ProjectCard
+                  imgPath={project.imgPath}
+                  title={project.title}
+                  description={project.description}
+                  demoLink={project.demoLink}
+                />
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
